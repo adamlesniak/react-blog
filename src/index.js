7d2f61a951842d96d779d51719a402f619ffac62
Adam Lesniak <adam@lesniak.pl>
2017-11-30 00:21:56 +0000

    Add in PostsShow

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import promise from 'redux-promise';
 
 import PostsIndex from './components/posts_index'
 import PostsNew from './components/posts_new'
+import PostsShow from './components/posts_show'
 
 import reducers from './reducers';
 
@@ -17,6 +18,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Switch>
         <Route path="/posts/new" component={PostsNew} />
+        <Route path="/posts/:id" component={PostsShow} />
         <Route path="/" component={PostsIndex} />
       </Switch>
     </BrowserRouter>
