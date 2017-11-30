06e6dc340b1a37454990aeae2d5261ecd4884127
Adam Lesniak <adam@lesniak.pl>
2017-11-30 23:52:04 +0000

    Post show component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions'
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const { post } = this.props;
+
+    if(!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts show!
+        <h3>{post.title}</h3>
+        <h6>Catgories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     )
   }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
