077c1bf0676a5493c8aece32fddc4d976da6adca
Adam Lesniak <adam@lesniak.pl>
2017-12-01 00:40:57 +0000

    Add in deletePost action creator and delete button.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions'
+import { fetchPost, deletePost } from '../actions'
 
 class PostsShow extends Component {
   componentDidMount() {
@@ -9,6 +9,11 @@ class PostsShow extends Component {
       const { id } = this.props.match.params;
       this.props.fetchPost(id);
     }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;    
+    this.props.deletePost(id, () => this.props.history.push('/'));
   }
 
   render() {
@@ -22,6 +27,7 @@ class PostsShow extends Component {
       <div>
         <div className="text-xs-right">
           <Link to="/" className="btn btn-primary">Back</Link>
+          <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>Delete Post</button>
         </div>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
@@ -35,4 +41,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
