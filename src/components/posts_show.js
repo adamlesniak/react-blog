311f38be46eec84cd74eec8fab96df5b0f51a0c5
Adam Lesniak <adam@lesniak.pl>
2017-12-01 00:05:06 +0000

    Link posts index to post show component.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions'
 
 class PostsShow extends Component {
   componentDidMount() {
-    const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    if(!this.props.post) {
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
   }
 
   render() {
@@ -17,8 +20,11 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <div className="text-xs-right">
+          <Link to="/" className="btn btn-primary">Back</Link>
+        </div>
         <h3>{post.title}</h3>
-        <h6>Catgories: {post.categories}</h6>
+        <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
       </div>
     )
